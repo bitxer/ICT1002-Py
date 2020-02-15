1d048a90ba8037735f996e2ad20f3f72bbabf96e
Joshua Lee <joshua.lee.99@hotmail.com>
2020-02-15 15:57:23 +0800

    GUI

---
+++ client/gui/start.py
@@ -0,0 +1,42 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtChart import QChart, QChartView, QPieSeries
+from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtCore import Qt
+
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        #Load the UI Page
+        uic.loadUi('main.ui', self)
+
+        self.create_piechart()
+
+    def create_piechart(self):
+        series = QPieSeries()
+        series.append("Bot", 10)
+        series.append("Brute Force -Web", 30)
+        series.append("Brute Force -XSS", 50)
+        series.append("DDOS attack-HOIC", 20)
+
+        chart = QChart()
+        chart.addSeries(series)
+        # chart.setAnimationOptions(QChart.SeriesAnimation)
+        chart.setTitle("Attacks")
+        
+        piechart1 = self.findChild(QChartView, "piechart1")
+        piechart1.setChart(chart)
+        piechart1.setRenderHint(QPainter.Antialiasing)
+
+
+def main():
+    app = QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':         
+    main()
