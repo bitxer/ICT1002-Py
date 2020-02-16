59ed196c6f7dcd1e7f14c88baefc5276bbe54b1f
Joshua Lee <joshua.lee.99@hotmail.com>
2020-02-17 00:15:13 +0800

    Data Table

--- client/gui/start.py
+++ client/gui/start.py
@@ -1,12 +1,15 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5.QtChart import QChart, QChartView, QPieSeries
+from PyQt5 import uic 
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidget,QTableWidgetItem, QHeaderView
+from PyQt5.QtChart import QChart, QChartView, QPieSeries, QPieSlice
 from PyQt5.QtGui import QPainter, QPen
 from PyQt5.QtCore import Qt
 from devtools import filedata
 import ast
-
 import sys
+from PyQt5.QtCore import QFile, QTextStream
+from PyQt5.QtWidgets import QApplication
+import breeze_resources
+import time
 
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
@@ -17,12 +20,13 @@ class MainWindow(QMainWindow):
         rawdata = filedata().strip("\n")
         rawdata = ast.literal_eval(rawdata)
 
-        data = DataHandler(rawdata)
+        self.data = DataHandler(rawdata)
 
-        summary = data.getSummary()
-        chartseries = data.getSeries()
+        self.summary = self.data.getSummary()
+        self.chartseries = self.data.getSeries()
 
-        self.displaychart("piechart1", chartseries, "Attack Types")
+        self.displaychart("attackchart", self.chartseries, "Attack Types")
+        self.displaytable("datatable")
 
     def displaychart(self, widgetname, chartseries, header):
         piechart = self.findChild(QChartView, widgetname)
@@ -30,14 +34,23 @@ class MainWindow(QMainWindow):
         piechart.setChart(chartdata)
         piechart.setRenderHint(QPainter.Antialiasing)
 
+    def displaytable(self, widgetname):
+        table = self.findChild(QTableWidget, widgetname)
+        display = DataTable(table, self.data.getData()).create()
+
+
+
 class DataHandler:
     def __init__(self, data):
         self.data = data
-        self.datahandler()
+        self.process()
 
-    def datahandler(self):
+    def process(self):
         summary = {
             'AtkCount' : 0,
+            'IP': {},
+            'Protocol': {},
+            'Port': {},
             'Atk' : {},
         }
 
@@ -51,6 +64,21 @@ class DataHandler:
                 summary['Atk'][v['Atk']] = 1
             else:
                 summary['Atk'][v['Atk']] += 1
+
+            if v['IP'] not in summary['IP']:
+                summary['IP'][v['IP']] = 1
+            else:
+                summary['IP'][v['IP']] += 1
+
+            if v['Protocol'] not in summary['Protocol']:
+                summary['Protocol'][v['Protocol']] = 1
+            else:
+                summary['Protocol'][v['Protocol']] += 1
+
+            if v['Port'] not in summary['Port']:
+                summary['Port'][v['Port']] = 1
+            else:
+                summary['Port'][v['Port']] += 1
 
         print(summary)
 
@@ -66,6 +94,47 @@ class DataHandler:
     def getSeries(self):
         return self.series
 
+    def getData(self):
+        return self.data
+
+class DataTable:
+    def __init__(self, tableobj, tabledata):
+        self.tableobj = tableobj
+        self.tabledata = tabledata
+
+    def create(self):
+        self.tableobj.setColumnCount(6)
+        self.tableobj.setRowCount(len(self.tabledata.keys()))
+        for k,v in self.tabledata.items():
+            index = k
+            rowcount = 0
+            # print(k)
+            # print(v)
+            for k,v in v.items():
+                if k == 'IsAtk':
+                    v = 'Yes'
+
+                if k == 'Time':
+                    v = time.strftime('%m/%d/%Y %H:%M:%S', time.gmtime(v))
+
+                self.tableobj.setItem(int(index), rowcount, QTableWidgetItem(str(v)))
+                # print(k, v)
+                rowcount += 1
+
+        # header = self.tableobj.horizontalHeader()       
+        # header.setSectionResizeMode(0, QHeaderView.ResizeToContents)
+        # header.setSectionResizeMode(1, QHeaderView.ResizeToContents)
+        # header.setSectionResizeMode(2, QHeaderView.ResizeToContents)
+        # header.setSectionResizeMode(3, QHeaderView.ResizeToContents)
+
+
+        # self.tableobj.setRowCount(4)
+        # self.tableobj.setColumnCount(5)
+
+        # self.tableobj.setItem(0,0, QTableWidgetItem("Cell (3,1)"))
+        # self.tableobj.setItem(0,1, QTableWidgetItem("Cell (3,2)"))
+        # self.tableobj.setItem(1,0, QTableWidgetItem("Cell (4,1)"))
+        # self.tableobj.setItem(1,1, QTableWidgetItem("Cell (4,2)"))
 
 class Piechart:
     def __init__(self, chartseries, title):
@@ -73,18 +142,31 @@ class Piechart:
         self.title = title
 
     def create(self):
+        slices = QPieSlice()
+        for x in range(0, len(self.chartseries.slices())):
+            slices = self.chartseries.slices()[x]
+            slices.setLabelVisible(True)
+
         chart = QChart()
         chart.addSeries(self.chartseries)
         chart.setAnimationOptions(QChart.SeriesAnimations)
         chart.setTitle(self.title)
-
+        chart.setTheme(5)
+        print("HI")
+        print(type(chart.theme()))
+        print("BYE")
         return chart
 
 
 def main():
     app = QApplication(sys.argv)
+    themefile = QFile(":/dark.qss")
+    themefile.open(QFile.ReadOnly | QFile.Text)
+    stream = QTextStream(themefile)
+    app.setStyleSheet(stream.readAll())
     main = MainWindow()
-    main.showMaximized()
+    # main.showMaximized()
+    main.show()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':         
