99b6a435a54cf9410d01fc216d6f20e326389def
Joshua Lee <joshua.lee.99@hotmail.com>
2020-02-18 17:04:28 +0800

    Top IPs and a whole lot of messy code

--- client/gui/start.py
+++ client/gui/start.py
@@ -1,7 +1,7 @@
 from PyQt5 import uic, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidget,QTableWidgetItem, QHeaderView
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidget,QTableWidgetItem, QHeaderView, QGraphicsSimpleTextItem, QListWidget
 from PyQt5.QtChart import QChart, QChartView, QPieSeries, QPieSlice
-from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtGui import QPainter, QPen, QMouseEvent
 from PyQt5.QtCore import Qt
 from devtools import filedata
 import ast
@@ -10,8 +10,9 @@ from PyQt5.QtCore import QFile, QTextStream
 from PyQt5.QtWidgets import QApplication
 import breeze_resources
 import time
+import pandas as pd
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         #Load the UI Page
@@ -20,24 +21,53 @@ class MainWindow(QtWidgets.QMainWindow):
         rawdata = filedata().strip("\n")
         rawdata = ast.literal_eval(rawdata)
 
-        self.data = DataHandler(rawdata)
-
+        # data
+        pandata = pd.DataFrame.from_dict(rawdata)
+        self.data = DataHandler(pandata)
         self.summary = self.data.getSummary()
         self.chartseries = self.data.getSeries()
+        
+        # upload
         self.actionUpload.triggered.connect(self.addItem)
         self.actionExit.triggered.connect(self.exit)
+
+        # displays
         self.displaychart("attackchart", self.chartseries, "Attack Types")
-        self.displaytable("datatable")
+        self.displaytable("datatable", self.data.getData())
+        # print(self.data.getData())
+        self.displaytop("toplist", self.data.gettopIPs())
+        # print(pd.DataFrame(self.data.topIPs(), index=[0]).transform)
+        # self.displaytable("toplist", pd.DataFrame(self.data.topIPs(), index=[0]))
 
     def displaychart(self, widgetname, chartseries, header):
-        piechart = self.findChild(QChartView, widgetname)
+        self.piechart = self.findChild(QChartView, widgetname)
         chartdata = Piechart(chartseries, header).create()
-        piechart.setChart(chartdata)
-        piechart.setRenderHint(QPainter.Antialiasing)
+        self.piechart.setChart(chartdata)
+        self.piechart.setRenderHint(QPainter.Antialiasing)
 
-    def displaytable(self, widgetname):
+    def displaytop(self, widgetname, data):
+        iptable = self.findChild(QTableWidget, widgetname)
+        iptable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        iptable.verticalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+        iptable.setColumnCount(2)
+        iptable.setRowCount(5)
+        index = 0
+        for k,v in data.items():
+            print(k,v)
+            iptable.setItem(int(index),0, QTableWidgetItem(k))
+            iptable.setItem(int(index),1, QTableWidgetItem(str(v)))
+            index += 1
+
+    def displaytable(self, widgetname, data):
         table = self.findChild(QTableWidget, widgetname)
-        display = DataTable(table, self.data.getData()).create()
+        DataTable(table, data).create()
+
+    # def displaylist(self, widgetname):
+    #     listview = self.findChild(QListWidget, widgetname)
+    #     for k,v in self.data.topIPs().items():
+    #         # listview.addItems()
+    #         print(k,v)
+
     
     def addItem(self):
         fileName, _ = QtWidgets.QFileDialog.getOpenFileName(None, "Select Text File", "", "Text Files (*.txt)")
@@ -52,6 +82,7 @@ class DataHandler:
     def __init__(self, data):
         self.data = data
         self.process()
+        self.topIPs()
 
     def process(self):
         summary = {
@@ -88,13 +119,22 @@ class DataHandler:
             else:
                 summary['Port'][v['Port']] += 1
 
-        print(summary)
+        # print(summary)
 
         for atk, val in summary['Atk'].items():
             series.append(atk, val)
 
         self.summary = summary
         self.series = series
+
+    def topIPs(self):
+        # top5list = sorted(self.summary['IP'], key=self.summary.get, reverse=True)
+        top = sorted(self.summary['IP'], key=self.summary['IP'].get, reverse=True)
+        output = {}
+        for x in top:
+            output[x] = self.summary['IP'][x]
+
+        self.topips = output
 
     def getSummary(self):
         return self.summary
@@ -105,13 +145,18 @@ class DataHandler:
     def getData(self):
         return self.data
 
+    def gettopIPs(self):
+        return self.topips
+
 class DataTable:
     def __init__(self, tableobj, tabledata):
         self.tableobj = tableobj
         self.tabledata = tabledata
+        self.tableobj.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.tableobj.verticalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
 
     def create(self):
-        self.tableobj.setColumnCount(6)
+        self.tableobj.setColumnCount(len(self.tabledata.values))
         self.tableobj.setRowCount(len(self.tabledata.keys()))
         for k,v in self.tabledata.items():
             index = k
@@ -132,6 +177,9 @@ class Piechart:
         self.chartseries = chartseries
         self.title = title
 
+    # def pieclick(self):
+    #     print("HI")
+
     def create(self):
         slices = QPieSlice()
         for x in range(0, len(self.chartseries.slices())):
@@ -143,6 +191,9 @@ class Piechart:
         chart.setAnimationOptions(QChart.SeriesAnimations)
         chart.setTitle(self.title)
         chart.setTheme(5)
+        chart.legend().setVisible(True)
+        chart.legend().setAlignment(Qt.AlignBottom)
+        chart.legend().attachToChart()
         return chart
 
 
@@ -153,8 +204,8 @@ def main():
     stream = QTextStream(themefile)
     app.setStyleSheet(stream.readAll())
     main = MainWindow()
-    # main.showMaximized()
-    main.show()
+    main.showMaximized()
+    # main.show()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':         
