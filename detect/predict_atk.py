0f6301631de3d5ac8a78b3d36abb251b9dd2319d
Ronnie-1910 <ronnietjw99@gmail.com>
2020-02-24 00:46:43 +0800

    clean up

--- detect/predict_atk.py
+++ detect/predict_atk.py
@@ -1,48 +1,94 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #Suppres error
-import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Suppress error
 from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import to_categorical, normalize
 import numpy as np
 import pandas as pd
-from sklearn.utils import shuffle
 import socket
-output = {}
-atks = {
- 0:'Bot',
- 1:'Brute Force -Web',
- 2:'Brute Force -XSS', 
- 3:'DDOS attack-HOIC',
- 4:'DDOS attack-LOIC-UDP',
- 5:'DoS attacks-GoldenEye',
- 6:'DoS attacks-Hulk', 
- 7:'DoS attacks-SlowHTTPTest', 
- 8:'DoS attacks-Slowloris',
- 9:'FTP-BruteForce',
- 10:'Infilteration',
- 11:'SQL Injection',
- 12:'SSH-Bruteforce'
-}
-protocols = {num:name[8:] for name,num in vars(socket).items() if name.startswith("IPPROTO")}
 
 
+def run_predict_isAtk(df):
+    '''
+    Separate attacks traffic and normal traffic
 
-def loadData(fileName):
-    dataFile = fileName
-    pickleDump = '{}.pickle'.format(dataFile)
-    if os.path.exists(pickleDump):
-        df = pd.read_pickle(pickleDump)
-    else:
-        df = pd.read_csv(dataFile)
-        df = df.dropna()
-        df.to_pickle(pickleDump)
-    return df
+    Parameters
+    ----------
+    df : DataFrame
 
-def load_model_csv():
-    model = load_model("Atk_multiclass_categorical_50_ep_80_bs.h5")
-    return model
+    Return
+    ------
+    [Success]
+        output : dict
+            Normal traffic
+        df2 : DataFrame
+            Attack traffic to be analyse
+    '''
+    output = {}
+    protocols = {num: name[8:] for name, num in vars(socket).items() if name.startswith("IPPROTO")}
+    columnList = df.columns
+    df1 = df.copy()
+    logID = df.pop('ID')
+    sourceIP = df.pop('SourceIP')
+    DestIP = df.pop('DestIP')
+    protocol = df['Protocol'].values
+    port_num = df['Dst Port'].values
+    time = df[['Timestamp']].values
+    df_test = normalize(df.values)
+    model = load_model("model/binary_class_classifier.model")
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    predictions = model.predict_proba(df_test)
 
-def run_predict(df):
+    df2 = pd.DataFrame(columns=columnList)
+    for p, q, r, s, t, sip, dip in zip(predictions, logID, protocol, port_num, time, sourceIP, DestIP):
+        if np.argmax(p) == 0:
+            output[int(q)] = {
+                "IsAtk": np.argmax(p),
+                "Source IP": sip,
+                "Dst IP": dip,
+                "Protocol": protocols[r],
+                "Dst Port": int(s),
+                "Time": int(t[0])
+            }
+
+
+        else:
+            row_index = df1.loc[df1["ID"] == q].index[0]
+            df2.loc[df1.index[row_index]] = df1.iloc[row_index]
+
+    return output, df2
+
+
+def run_predict_Atks(df):
+    '''
+    Identify Possible Attacks
+
+    Parameters
+    ----------
+    df : DataFrame
+
+    Return
+    ------
+        output : dict
+            Labels Attack Traffic
+	'''
+
+    output = {}
+    protocols = {num: name[8:] for name, num in vars(socket).items() if name.startswith("IPPROTO")}
+    atks = {
+        0: 'Bot',
+        1: 'Brute Force -Web',
+        2: 'Brute Force -XSS',
+        3: 'DDOS attack-HOIC',
+        4: 'DDOS attack-LOIC-UDP',
+        5: 'DoS attacks-GoldenEye',
+        6: 'DoS attacks-Hulk',
+        7: 'DoS attacks-SlowHTTPTest',
+        8: 'DoS attacks-Slowloris',
+        9: 'FTP-BruteForce',
+        10: 'Infilteration',
+        11: 'SQL Injection',
+        12: 'SSH-Bruteforce'
+    }
 
     logID = df.pop('ID')
     SourceIP = df.pop('SourceIP')
@@ -51,30 +97,42 @@ def run_predict(df):
     protocol = df[['Protocol']].values
     port = df[['Dst Port']].values
     df_test = normalize(df.values)
-
-    model = load_model_csv()
+    model = load_model("model/Atk_multiclass_categorical_50_ep_80_bs.h5")
     model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
 
     predictions = model.predict_proba(df_test)
-    i=0
-    proto = dict()
-    for l,sip,dip,p1,p2,p3,t in zip(logID,SourceIP,DestIP,predictions,protocol,port,time):#ID
+    for l, sip, dip, p1, p2, p3, t in zip(logID, SourceIP, DestIP, predictions, protocol, port, time):
         output[int(l)] = {"IsAtk": 1,
-                     "Source IP":sip,
-                     "Dst IP": dip,
-                     "Protocol":protocols[int(p2)],
-                     "Dst Port":int(p3),
-                     "Atk": atks[np.argmax(p1)],
-                     "Time":int(t[0])
-                     }
-
-        i+=1
-
+                          "Source IP": sip,
+                          "Dst IP": dip,
+                          "Protocol": protocols[int(p2)],
+                          "Dst Port": int(p3),
+                          "Atk": atks[np.argmax(p1)],
+                          "Time": int(t[0])
+                          }
     return output
 
-if __name__=="__main__":
-    output = run_predict("big_test_3.csv")
-    print(output)
+
+def run_predict_main(df):
+    '''
+    To identify attacks from log data
+
+    Parameters
+    ----------
+    df : DataFrame
+
+    Return
+    ------
+    [Success]
+        output : dict
+            Results of the logs
+
+	'''
+    (noAtk, Atk) = run_predict_isAtk(df)
+    output = noAtk.copy()
+    atk_output = run_predict_Atks(Atk)
+
+    output.update(atk_output)
+    return output
