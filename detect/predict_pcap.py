f3592eee185b229a523482bbf2904c2d3dd4dc5b
Ronnie-1910 <ronnietjw99@gmail.com>
2020-02-23 13:04:03 +0800

    add combine predict

--- detect/predict_pcap.py
+++ detect/predict_pcap.py
@@ -3,8 +3,6 @@ import predict_benign
 import pandas as pd
 
 def run_predict(df):
-    noAtk = {11:{'IsAtk':0, 'Time': 12345}}
-
     (noAtk,Atk) = predict_benign.run_predict(df)
     output = noAtk.copy()
     atk_output = predict_atk.run_predict(Atk)
@@ -15,4 +13,4 @@ def run_predict(df):
 
 
 if __name__ == "__main__":
-    print(run_predict("big_test_5.csv"))
+    print(run_predict("sample.csv"))
