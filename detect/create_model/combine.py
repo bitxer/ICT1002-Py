a1d6e8399ba7f8579c31195892aa0783e7cb9e03
Yong Ze <24569832+bitxer@users.noreply.github.com>
2020-02-25 12:09:32 +0800

    Tensor flow ml (#5)
    Added ml functionality

---
+++ detect/create_model/combine.py
@@ -0,0 +1,68 @@
+import os
+import pandas as pd
+from sklearn.utils import shuffle
+import sys
+
+labels = {'Benign': 0, 'FTP-BruteForce': 1, 'SSH-Bruteforce': 1, 'DoS attacks-GoldenEye': 1, 'DoS attacks-Slowloris': 1,
+     'DoS attacks-SlowHTTPTest': 1, 'DoS attacks-Hulk': 1, 'Brute Force -Web': 1, 'Brute Force -XSS': 1,
+     'SQL Injection': 1, 'Infilteration': 1, 'Bot': 1,'DDOS attack-LOIC-UDP':1}
+
+
+def merge_csv(fileNames, dataPath):
+    '''
+    Combines Dataset into one csv
+    Parameters
+    ----------
+    fileNames : list(str)
+    dataPath : Directory Location
+
+    Return
+    ------
+    [Success]
+        df : DataFrame
+            Shuffled combine dataset
+    '''
+    df = pd.read_csv(os.path.join(dataPath, fileNames[0]))
+    for name in fileNames[1:]:
+        fname = os.path.join(dataPath, name)
+        print('Appending', fname)
+        df1 = pd.read_csv(fname)
+        df = df.append(df1, ignore_index=True)
+
+    return shuffle(df)
+def combine_main(fileDir, LabelType):
+    '''
+    Main Function to run multi-class or binary labeling
+
+    Parameters
+    ----------
+    fileDir : str
+    LabelType : int
+    '''
+
+    dataPath = fileDir # Dataset folder in csv
+    fileNames = os.listdir(dataPath)
+    df = merge_csv(fileNames,dataPath)
+    if str(LabelType).isdigit():
+        if LabelType == 0:
+            print('create combined attacks')
+            outFile = os.path.join(dataPath, 'combined_attacks')
+            df.to_csv(outFile + '.csv', index=False)
+            print('Combine Dataset Completed')
+
+
+        elif LabelType ==1:
+            print('creating combine csv file')
+
+            for k,v in labels.items():
+                df.loc[df['Label']==k, 'Label'] = v
+
+            outFile = os.path.join(dataPath, 'combine_atk_or_not_csv')
+            df.to_csv(outFile, index=False)
+            print('Combine Dataset Completed')
+
+        else:
+            print("Label_type: 1 for binary labels, 0 for keep labels")
+    else:
+        print("Label_type: 1 for binary labels, 0 for keep labels")
+
---
+++ detect/predict_atk.py
@@ -0,0 +1,134 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Suppress error
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import to_categorical, normalize
+import numpy as np
+import pandas as pd
+import socket
+
+
+def run_predict_isAtk(df):
+    '''
+    Separate attacks traffic and normal traffic
+
+    Parameters
+    ----------
+    df : DataFrame
+
+    Return
+    ------
+    [Success]
+        output : dict
+            Normal traffic
+        df2 : DataFrame
+            Attack traffic to be analyse
+    '''
+    output = {}
+    protocols = {num: name[8:] for name, num in vars(socket).items() if name.startswith("IPPROTO")}
+    columnList = df.columns
+    df1 = df.copy()
+    logID = df.pop('ID')
+    sourceIP = df.pop('SourceIP')
+    protocol = df['Protocol'].values
+    port_num = df['Dst Port'].values
+    time = df[['Timestamp']].values
+    df_test = normalize(df.values)
+    model = load_model("model/binary_class_classifier.h5")
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    predictions = model.predict_proba(df_test)
+
+    df2 = pd.DataFrame(columns=columnList)
+    for p, q, r, s, t, sip in zip(predictions, logID, protocol, port_num, time, sourceIP):
+        if np.argmax(p) == 0:
+            output[int(q)] = {
+                "IsAtk": np.argmax(p),
+                "IP": sip,
+                "Protocol": protocols[r],
+                "Port": int(s),
+                "Time": int(t[0])
+            }
+
+
+        else:
+            row_index = df1.loc[df1["ID"] == q].index[0]
+            df2.loc[df1.index[row_index]] = df1.iloc[row_index]
+
+    return output, df2
+
+
+def run_predict_Atks(df):
+    '''
+    Identify Possible Attacks
+
+    Parameters
+    ----------
+    df : DataFrame
+
+    Return
+    ------
+        output : dict
+            Labels Attack Traffic
+	'''
+
+    output = {}
+    protocols = {num: name[8:] for name, num in vars(socket).items() if name.startswith("IPPROTO")}
+    atks = {
+        0: 'Bot',
+        1: 'Brute Force -Web',
+        2: 'Brute Force -XSS',
+        3: 'DDOS attack-HOIC',
+        4: 'DDOS attack-LOIC-UDP',
+        5: 'DoS attacks-GoldenEye',
+        6: 'DoS attacks-Hulk',
+        7: 'DoS attacks-SlowHTTPTest',
+        8: 'DoS attacks-Slowloris',
+        9: 'FTP-BruteForce',
+        10: 'Infilteration',
+        11: 'SQL Injection',
+        12: 'SSH-Bruteforce'
+    }
+
+    logID = df.pop('ID')
+    SourceIP = df.pop('SourceIP')
+    time = df[['Timestamp']].values
+    protocol = df[['Protocol']].values
+    port = df[['Dst Port']].values
+    df_test = normalize(df.values)
+    model = load_model("model/Atk_multiclass_categorical_50_ep_80_bs.h5")
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    predictions = model.predict_proba(df_test)
+    for l, sip, p1, p2, p3, t in zip(logID, SourceIP, predictions, protocol, port, time):
+        output[int(l)] = {"IsAtk": 1,
+                          "IP": sip,
+                          "Protocol": protocols[int(p2)],
+                          "Port": int(p3),
+                          "Atk": atks[np.argmax(p1)],
+                          "Time": int(t[0])
+                          }
+    return output
+
+
+def run_predict_main(df):
+    '''
+    To identify attacks from log data
+
+    Parameters
+    ----------
+    df : DataFrame
+
+    Return
+    ------
+    [Success]
+        output : dict
+            Results of the logs
+
+	'''
+    (noAtk, Atk) = run_predict_isAtk(df)
+    output = noAtk.copy()
+    atk_output = run_predict_Atks(Atk)
+
+    output.update(atk_output)
+    return output
