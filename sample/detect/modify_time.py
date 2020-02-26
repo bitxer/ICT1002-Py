795ebf9e42da37f250b50808102b54c71615a796
Joshua Lee <joshua.lee.99@hotmail.com>
2020-02-26 22:58:54 +0800

    samples

---
+++ sample/detect/modify_time.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def main():
+    fname = ''
+    delta = 31536000 * 2
+    csv = pd.read_csv(fname)
+    csv['Timestamp'] += delta
+    csv.to_csv(fname, header=True, index=False)
+
+if __name__ == '__main__':
+    main()
