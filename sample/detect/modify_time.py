d57c8d484f921c18d85d4651dbf7a005fe3c0b1e
bitxer <24569832+bitxer@users.noreply.github.com>
2020-02-26 22:04:50 +0800

    add spaces

--- sample/detect/modify_time.py
+++ sample/detect/modify_time.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def main():
     fname = ''
-    delta = 31536000*2
+    delta = 31536000 * 2
     csv = pd.read_csv(fname)
     csv['Timestamp'] += delta
     csv.to_csv(fname, header=True, index=False)
